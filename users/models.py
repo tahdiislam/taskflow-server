98bbb8720c5dfe3987ac018e98519f736436873e
Tahdi Islam <tshdi60@gmail.com>
2024-09-24 05:32:49 +0600

    [minor] [Done] remove role from customer model

--- users/models.py
+++ users/models.py
@@ -1,20 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-ADMIN = 'ADMIN'
-MANAGER = 'MANAGER'
-MEMBER = 'MEMBER'
-
-ROLE_CHOICES = [
-        (MEMBER, 'Member'),
-        (MANAGER, 'Manager'),
-        (ADMIN, 'Admin'),
-        ]
-
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=MEMBER)
     profile_picture = models.ImageField(upload_to="users/images", blank=True, null=True)
     task_completed = models.PositiveIntegerField(default=0)
     job_title = models.CharField(max_length=100, blank=True, null=True)
