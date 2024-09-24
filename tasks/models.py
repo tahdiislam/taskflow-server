bad00433853e6980e4b632c4b5811f161f4725e5
Tahdi Islam <tshdi60@gmail.com>
2024-09-24 09:34:44 +0600

    [Done] Add ActivityLog model

--- tasks/models.py
+++ tasks/models.py
@@ -36,3 +36,12 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+class ActivityLog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, related_name='activity_logs', on_delete=models.CASCADE)
+    action = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.action
