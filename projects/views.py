f562e2c1056bea0ea24a0182db19b2a2bb4803c3
Tahdi Islam <tshdi60@gmail.com>
2024-09-29 00:39:29 +0600

    [Done] add extra query project all viewset

--- projects/views.py
+++ projects/views.py
@@ -12,3 +12,9 @@ class ProjectViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         project = serializer.save()
         ProjectRole.objects.create(user=project.created_by, project=project, role='Admin')
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user_id = self.request.query_params.get('user_id')
+        if user_id is not None:
+            queryset = queryset.filter(created_by=user_id)
+        return queryset
